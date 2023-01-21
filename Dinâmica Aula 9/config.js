588f91951d4fd8b20c4d4be8c84768f0e54c6526
Juliano dos Santos <109418172+Julianods11@users.noreply.github.com>
2023-01-21 18:17:15 -0300

    Funcionalidades
    Adicionando funcionalidades requisitadas pelo projeto da DNC.

---
+++ Dinâmica Aula 9/config.js
@@ -0,0 +1,43 @@
+function abrir1(){
+    var button1 = document.getElementById("buttonModal1")
+
+    button1.style.display = 'block'
+}
+function fechar1(){
+    var button1 = document.getElementById("buttonModal1")
+
+    button1.style.display = 'none'
+}
+
+function abrir2(){
+    var button2 = document.getElementById("buttonModal2")
+
+    button2.style.display = 'block'
+}
+function fechar2(){
+    var button2 = document.getElementById("buttonModal2")
+
+    button2.style.display = 'none'
+}
+
+function abrir3(){
+    var button3 = document.getElementById("buttonModal3")
+
+    button3.style.display = 'block'
+}
+function fechar3(){
+    var button3 = document.getElementById("buttonModal3")
+
+    button3.style.display = 'none'
+}
+
+function abrir4(){
+    var button4 = document.getElementById("buttonModal4")
+
+    button4.style.display = 'block'
+}
+function fechar4(){
+    var button4 = document.getElementById("buttonModal4")
+
+    button4.style.display = 'none'
+}
